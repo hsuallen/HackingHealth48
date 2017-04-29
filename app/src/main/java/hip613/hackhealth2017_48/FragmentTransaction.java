da88c0de85547b80f2856eadd5e0deb14f32148f
Allen Hsu <hsu.y.allen@gmail.com>
2017-04-29 14:39:50 -0400

    Added fragment layout and java file

--- app/src/main/java/hip613/hackhealth2017_48/FragmentTransaction.java
+++ app/src/main/java/hip613/hackhealth2017_48/FragmentTransaction.java
@@ -1,5 +1,6 @@
 package hip613.hackhealth2017_48;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -11,5 +12,8 @@ public class FragmentTransaction extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Fragment fragment = null;
+
     }
 }
