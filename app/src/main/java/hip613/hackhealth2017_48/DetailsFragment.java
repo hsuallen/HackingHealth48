d653e1d60a3b959b1dd51b7cb4d88a568e2a8b75
Allen Hsu <hsu.y.allen@gmail.com>
2017-04-29 20:36:24 -0400

    Image displaying almost working

--- app/src/main/java/hip613/hackhealth2017_48/DetailsFragment.java
+++ app/src/main/java/hip613/hackhealth2017_48/DetailsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -13,7 +14,8 @@ import android.widget.TextView;
 
 public class DetailsFragment extends Fragment {
     private int upvotes;
-    private String title, category, description;
+    private ImageView image;
+    private String title, category, description, URL;
     private TextView titleTV, categoryTV, descTV;
     private View gui;
 
@@ -26,16 +28,19 @@ public class DetailsFragment extends Fragment {
         title = bun.getString("title");
         category = bun.getString("category");
         description = bun.getString("description");
+        URL = bun.getString("imageURL");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         gui = inflater.inflate(R.layout.post_details, null);
 
+        image = (ImageView)gui.findViewById(R.id.imageView3);
         titleTV = (TextView)gui.findViewById(R.id.textView7);
         categoryTV = (TextView)gui.findViewById(R.id.textView9);
         descTV = (TextView)gui.findViewById(R.id.textView11);
 
+        image.setImageBitmap(HTTPUtils.getImage("http://res.cloudinary.com/allenhsu/image/upload/v1493508293/igmkahqqvmh09c3abj58.png"));
         titleTV.setText(title);
         categoryTV.setText(category);
         descTV.setText(description);
